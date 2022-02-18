ca056d441fa0c025f769574aea5456bba59fb6bd
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-02-18 00:54:48 -0300

    fix req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -2,18 +2,14 @@ class Queue:
     def __init__(self):
         self.data = []
 
-
     def __len__(self):
         return len(self.data)
-
 
     def enqueue(self, value):
         return self.data.append(value)
 
-
     def dequeue(self):
         return self.data.pop(0)
-
 
     def search(self, index):
         if len(self.data) > index >= 0:

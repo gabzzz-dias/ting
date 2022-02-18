c59b57f71d01d278e0c70cc0d1e1ed7de933e38e
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-02-18 05:01:33 -0300

    req6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
+def search(word, arr):
+    result = []
+    for i, j in enumerate(arr):
+        if word in j:
+            result.append({'linha': i + 1})
+
+    return result
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in instance._data:
+        file_lines = search(word, i['linhas_do_arquivo'])
+
+        if len(file_lines):
+            result.append({
+                'palavra': word,
+                'arquivo': i['nome_do_arquivo'],
+                'ocorrencias': file_lines
+            })
+
+    return result
 
 
 def search_by_word(word, instance):

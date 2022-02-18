9a08b9973b7f30b83c72b21b41ce1f75c03b3948
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-02-18 04:31:55 -0300

    pfv

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,29 +1,21 @@
-from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 import sys
 
 
-def search(path_file, instance):
-    if instance.__len__():
-        for i in instance._data:
-            return i['nome_do_arquivo'] == path_file
-    else:
-        return False
-
-
 def process(path_file, instance):
-    process_file = txt_importer(path_file)
+    for i in range(len(instance.data)):
+        if path_file == instance.search(i)["nome_do_arquivo"]:
+            return None
+
+    file = txt_importer(path_file)
     result = {
         "nome_do_arquivo": path_file,
-        "qtd_linhas": len(process_file),
-        "linhas_do_arquivo": process_file
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
     }
-    repeated = search(path_file, instance)
 
-    if repeated is False:
-        instance.enqueue(result)
-
-    sys.stdout.write(f'{result}\n')
+    instance.enqueue(result)
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
@@ -32,10 +24,3 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
-
-
-queue = Queue()
-process('statics/arquivo_teste.txt', queue)
-process('statics/arquivo_teste.txt', queue)
-
-remove(queue)
